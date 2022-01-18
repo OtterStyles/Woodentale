2a162428f29ddcfaf05595d52e0dbf3edf194743
WieserJulian <julian.wieser@i-wieser.com>
2022-01-18 07:56:12 +0100

    clonedPlayerSkript

--- Woodentale/Skript/Player.cs
+++ Woodentale/Skript/Player.cs
@@ -1,22 +1,46 @@
 using Godot;
-//using System;
+using System;
 
-public class Player : KinematicBody2D
+public class Player : Godot.KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	Vector2 _velocity = Vector2.Zero;
+	private const int MAX_SPEED = 80;
+	private const int ACCELERATION = 500;
+	const int FRICTION = 600;
 
-	// Called when the node enters the scene tree for the first time.
+	private AnimationPlayer _animationPlayer = null;
+	private AnimationTree _animationTree = null;
+	private AnimationNodeStateMachinePlayback _animationState = null;
+	
 	public override void _Ready()
 	{
-		GD.Print("Starts");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_animationTree = GetNode<AnimationTree>("AnimationTree");
+		_animationState = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
 	}
+	
+	
+	public override void _PhysicsProcess(float delta)
+	{
+		Vector2 inputVector = Vector2.Zero;
+		inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+		inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+		inputVector = inputVector.Normalized();
+		
+		if (inputVector != Vector2.Zero)
+		{
+			_animationTree.Set("parameters/Idle/blend_position", inputVector);
+			_animationTree.Set("parameters/Run/blend_position", inputVector);
+			_animationState.Travel("Run");
+			_velocity = _velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
+		}
+		else
+		{
+			_animationState.Travel("Idle");
+			_velocity = _velocity.MoveToward(Vector2.Zero, FRICTION * delta);
+		}
+		_velocity = MoveAndSlide(_velocity);
+		
+	}
 }
-
