f0c1b11903747e16a965fe3b9dabdf04d31c8fd6
WieserJulian <julian.wieser@i-wieser.com>
2022-02-15 10:18:25 +0100

    test

---
+++ Woodentale/Skript/Hurtbox.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class Hurtbox : Area2D
+{
+	[Export] public bool showHit = true;
+	[Export] public PackedScene HitEffect = (PackedScene) ResourceLoader.Load("res://PreFab/Effects/HitEffect.tscn");
+
+
+	public void _on_Hurtbox_area_entered(Area area)
+	{
+		if(showHit)
+		{
+			var effect = (Node2D) HitEffect.Instance();
+			var main = GetTree().CurrentScene;
+			main.AddChild(effect);
+			effect.GlobalPosition = GlobalPosition;
+		}
+	}
+}
