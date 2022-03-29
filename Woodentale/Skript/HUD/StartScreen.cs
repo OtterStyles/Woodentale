c63572c169c71d45ce541d652e1a964fe250723a
WieserJulian <julian.wieser@i-wieser.com>
2022-03-29 19:06:30 +0200

    Starting Menue

---
+++ Woodentale/Skript/HUD/StartScreen.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class StartScreen : Control
+{
+    public PackedScene OptionsGUI = ResourceLoader.Load<PackedScene>("res://Menu/Options.tscn");
+    public PackedScene StartScene = ResourceLoader.Load<PackedScene>("res://Scene/TestHub.tscn");
+
+
+    public void _on_Start_pressed()
+    {
+        GetTree().ChangeSceneTo(StartScene);
+    }
+
+    public void _on_Options_pressed()
+    {
+        GetTree().ChangeSceneTo(OptionsGUI);
+    }
+
+    public void _on_Exit_pressed()
+    {
+        GetTree().Quit(1);
+    }
+}
