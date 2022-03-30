a58ec66859b656b6d87beec560e703391c49e2ab
WieserJulian <julian.wieser@i-wieser.com>
2022-03-30 08:40:15 +0200

    Adapt Menue of Woodentale

--- Woodentale/Skript/HUD/StartScreen.cs
+++ Woodentale/Skript/HUD/StartScreen.cs
@@ -3,22 +3,27 @@ using System;
 
 public class StartScreen : Control
 {
-    public PackedScene OptionsGUI = ResourceLoader.Load<PackedScene>("res://Menu/Options.tscn");
-    public PackedScene StartScene = ResourceLoader.Load<PackedScene>("res://Scene/TestHub.tscn");
+	public PackedScene OptionsGUI = ResourceLoader.Load<PackedScene>("res://Menu/Options.tscn");
+	public PackedScene StartScene = ResourceLoader.Load<PackedScene>("res://Scene/TestHub.tscn");
+	private Options _options = null;
+	
+	public override void _Ready()
+	{
+		_options = GetNode<Options>("MarginContainer/Options");
+	}
 
+	public void _on_Start_pressed()
+	{
+		GetTree().ChangeSceneTo(StartScene);
+	}
 
-    public void _on_Start_pressed()
-    {
-        GetTree().ChangeSceneTo(StartScene);
-    }
+	public void _on_Options_pressed()
+	{
+		_options.Visible = true;
+	}
 
-    public void _on_Options_pressed()
-    {
-        GetTree().ChangeSceneTo(OptionsGUI);
-    }
-
-    public void _on_Exit_pressed()
-    {
-        GetTree().Quit(1);
-    }
+	public void _on_Exit_pressed()
+	{
+		GetTree().Quit(1);
+	}
 }
