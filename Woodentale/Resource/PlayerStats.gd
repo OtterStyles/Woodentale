90821b3b8f6bb2bd7be871726d415209b2dc5810
WieserJulian <julian.wieser@i-wieser.com>
2022-07-11 22:26:27 +0200

    Simplest Movements

---
+++ Woodentale/Resource/PlayerStats.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name PlayerStats
+
+enum Movement {
+	Speed = 400,
+	MaxSpeed = 500,
+	Friction = 40
+}
+
---
+++ Woodentale/Skripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+	
+	
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -PlayerStats.Movement.Speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x =  PlayerStats.Movement.Speed
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -PlayerStats.Movement.Speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = PlayerStats.Movement.Speed
+		
+	move_and_slide(velocity)
+	
