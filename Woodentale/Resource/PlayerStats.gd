4260c7817454dfccade77881043f70858747177c
WieserJulian <julian.wieser@i-wieser.com>
2022-07-13 07:45:03 +0200

    Tweek the PlayerSpeed and smoothen Camera

--- Woodentale/Resource/PlayerStats.gd
+++ Woodentale/Resource/PlayerStats.gd
@@ -4,7 +4,13 @@ class_name PlayerStats
 
 enum Movement {
 	Speed = 400,
-	MaxSpeed = 500,
-	Friction = 40
+	Sprint = 600,
+	Acceleration = 50,
+	Friction = 1
+}
+
+enum Multiplier {
+	SpeedAbs = 0,
+	SpeedPerc = 1
 }
 
--- Woodentale/Skripts/Player.gd
+++ Woodentale/Skripts/Player.gd
@@ -1,15 +1,31 @@
 extends KinematicBody2D
-	
-	
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -PlayerStats.Movement.Speed
-	if Input.is_action_pressed("ui_right"):
-		velocity.x =  PlayerStats.Movement.Speed
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -PlayerStats.Movement.Speed
-	if Input.is_action_pressed("ui_down"):
-		velocity.y = PlayerStats.Movement.Speed
 
-	move_and_slide(velocity)
+# Movement
+var velocity = Vector2()
+var speed = PlayerStats.Movement.Speed
+
+
+func getInput():
+	var input = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		input.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		input.x += 1
+	if Input.is_action_pressed("ui_up"):
+		input.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		input.y += 1
+	return input
+
+func calculateStats():
+	speed = (PlayerStats.Movement.Speed + PlayerStats.Multiplier.SpeedAbs) * PlayerStats.Multiplier.SpeedPerc 
+
+
+func _process(delta):
+	calculateStats()
+	var direction = getInput()
+	if direction.length() > 0:
+		velocity = lerp(velocity, direction * speed, PlayerStats.Movement.Acceleration)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, PlayerStats.Movement.Friction)
+	velocity = move_and_slide(velocity * delta)
