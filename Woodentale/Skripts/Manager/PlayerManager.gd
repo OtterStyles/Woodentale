3843c90491f3a54b123332461787a9b67b594026
WieserJulian <julian.wieser@i-wieser.com>
2022-08-07 12:55:09 +0200

    PlayerManager.gd aktualisiert

--- Woodentale/Skripts/Manager/PlayerManager.gd
+++ Woodentale/Skripts/Manager/PlayerManager.gd
@@ -1,13 +1,5 @@
 extends Node
 
-@export var SPEEDABS: float = 0
-@export var SPEEDPERC: float = 1
-@export var SPEED_DEFAULT: int = 300
-@export var SPEED_SPRINT_MULT: float = 1.2
-@export var ACCELERATION: float = 1000
-@export var FRICTION: float = 1500
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
