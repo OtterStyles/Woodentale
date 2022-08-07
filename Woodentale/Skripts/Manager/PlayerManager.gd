cadde2022ef7ed1dd1a5420c4e5bdcc90021fa6b
WieserJulian <julian.wieser@i-wieser.com>
2022-08-07 12:53:30 +0200

    Init PlayerManager

---
+++ Woodentale/Skripts/Manager/PlayerManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var SPEEDABS: float = 0
+@export var SPEEDPERC: float = 1
+@export var SPEED_DEFAULT: int = 300
+@export var SPEED_SPRINT_MULT: float = 1.2
+@export var ACCELERATION: float = 1000
+@export var FRICTION: float = 1500
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
